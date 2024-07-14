75ddde6f72fad183797e3fe47479d33310af8cb1
Paweł <pwalig03@gmail.com>
2024-07-14 14:58:33 +0200

    Fix crosshair_indicator after UI update

--- src/crosshair_indicator.cpp
+++ src/crosshair_indicator.cpp
@@ -4,7 +4,7 @@
 
 game::crosshair_indicator::crosshair_indicator(const std::string& img) :
 	ft(1.0f, std::bind(&scripts_system::safe_destroy, this)),
-	uii(img, glm::vec3(960.0f, 540.0f, -500.0f), glm::scale(glm::mat4(1.0f), glm::vec3(30.0f)))
+	uii(img, glm::vec3(0.5f, 0.5f, 0.51f), glm::scale(glm::mat4(1.0f), glm::vec3(0.029f)))
 {}
 
 void game::crosshair_indicator::update()
